d69701f3658086a0d0f2109a4ca0a3b40f1f9da8
Ramakrishna900 <ramakrishnagompala1@gmail.com>
2024-01-10 07:37:35 +0530

    Js_stopawatch

---
+++ index.js
@@ -0,0 +1,37 @@
+
+const startEl =document.getElementById("start");
+const stopEl =document.getElementById("stop");
+const resetEl =document.getElementById("reset");
+const timerEl =document.getElementById("timer");
+
+let interval;
+let timeLeft=1500;
+
+function updateTimer(){
+  let minutes=Math.floor(timeLeft/60)
+  let seconds=timeLeft%60
+  let formattedTime= `${minutes.toString().padStart(2,"0")}: ${seconds.toString().padStart(2,"0")}`;
+  timerEl.textContent=formattedTime
+}
+function startTimer(){
+interval =setInterval(()=>{
+  timeLeft--;
+  updateTimer();
+  if(timeLeft===0){
+    clearInterval(interval);
+    alert("Time is Over!");
+  }
+},1000)
+ 
+}
+function stopTimer(){
+  clearInterval(interval);
+}
+function resetTimer(){
+  timeLeft=1500;
+  clearInterval(interval);
+  updateTimer();
+}
+startEl.addEventListener("click",startTimer)
+stopEl.addEventListener("click",stopTimer)
+resetEl.addEventListener("click",resetTimer)
